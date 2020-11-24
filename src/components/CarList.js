701aedc35f5be8867e32b57db6bc6e1f7d1971f7
SlawomirJurak <sljrk@interia.eu>
2020-11-24 21:59:53 +0100

    add filtering car list by year of production

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,19 +1,52 @@
 import React, {Component} from "react";
 import CarListHeader from "./CarListHeader";
 import CarLine from "./CarLine";
-import FilterForm from "./FilterForm";
 
 class CarList extends Component {
 
     state = {
-        cars: []
+        cars: [],
+        yearFrom: 0,
+        yearTo: 0,
+        redirect: false
     }
 
-    async componentDidMount() {
-        let cars = await fetch('http://localhost:8080/cars');
+    componentDidMount() {
+        this.fetchCars();
+    }
+
+    fetchCars = async () => {
+        let yearFrom = this.state.yearFrom;
+        let yearTo = this.state.yearTo;
+        let url;
+
+        if (yearFrom > 0 && yearTo > 0) {
+            url = 'http://localhost:8080/cars/' + yearFrom + '/' + yearTo;
+        } else {
+            url = 'http://localhost:8080/cars';
+        }
+
+        let cars = await fetch(url);
 
         cars = await cars.json();
         this.setState({cars});
+    }
+
+    handleFilter = (event) => {
+        this.fetchCars();
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleResetFilter = (event) => {
+        this.setState({
+            yearFrom: 0,
+            yearTo: 0
+        }, this.fetchCars);
     }
 
     render() {
@@ -21,7 +54,16 @@ class CarList extends Component {
         return (
             <div>
                 <h1>Car list</h1>
-                <FilterForm/>
+                <div className={'form-inline'}>
+                    <label>Year from</label>
+                    <input type={'number'} className={'form-control mb-2 mr-sm-2'} name={'yearFrom'}
+                           value={this.state.yearFrom} onChange={this.handleChange}/>
+                    <label>Year to</label>
+                    <input type={'number'} className={'form-control mb-2 mr-sm-2'} name={'yearTo'}
+                           value={this.state.yearTo} onChange={this.handleChange}/>
+                    <button className="btn btn-primary mb-2" onClick={this.handleFilter}>Filter</button>
+                    <button className="btn btn-primary mb-2" onClick={this.handleResetFilter}>Reset filter</button>
+                </div>
                 <table className={'table'}>
                     <CarListHeader/>
                     {this.state.cars.length > 0 && <tbody>
